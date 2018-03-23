d72e310e4caf9e5a4fe418800f222eb593227ccf
Daniel <danicunha97@gmail.com>
2018-03-23 15:41:21 +0000

    PrescriçãoAtualizada

--- HospitalCare/src/Prescrição.java
+++ HospitalCare/src/Prescrição.java
@@ -1,4 +1,28 @@
+import java.util.ArrayList;
+import java.util.Date;
 
 public class Prescrição {
+	
+	protected Date data;
+	
+	protected Doente doente;
+	protected Medicamento medicamento;
+	protected Médico medico;
+	protected ArrayList<HorarioToma> horarios;
+	
+	public Prescrição()
+	{
+		horarios = new ArrayList<HorarioToma>();
+	}
 
+	public Prescrição(Date data, Doente doente, Medicamento medicamento, Médico medico,
+			ArrayList<HorarioToma> horarios) {
+		this.data = data;
+		this.doente = doente;
+		this.medicamento = medicamento;
+		this.medico = medico;
+		this.horarios = horarios;
+	}
+	
+	
 }
